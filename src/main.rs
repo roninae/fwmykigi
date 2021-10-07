c962fcbcc36edd8a546b8958de9ce309b6e73214
Mak <m@ronin.ae>
2021-10-07 08:24:39 +0400

    fixed wasm call

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,16 @@ fn main() {
     // The YAML file is found relative to the current file, similar to how modules are found
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-
-    let wasm_bytes = std::fs::read("/Users/mak/Desktop/demo.wasm").unwrap();
+    let wasm_file = matches.value_of("wasm").unwrap();
+    let wasm_bytes = std::fs::read(wasm_file).unwrap();
     let store = Store::default();
     let module = Module::new(&store, wasm_bytes).unwrap();
     let import_object = imports! {};
     let instance = Instance::new(&module, &import_object).unwrap();
-
-    let add_one = instance.exports.get_function("add_one").unwrap().native::<i32, i32>().unwrap();
-
-    println!("Calling `add_one` function...");
-    let result = add_one.call(3).unwrap();
-
-    println!("Results of `add_one`: {:?}", result);
+    let fab_events = instance.exports.get_function("fab_events").unwrap().native::<i32, i32>().unwrap();
+    println!("Calling `fab_events` function with default params [10000]...");
+    let result = fab_events.call(10000).unwrap();
+    println!("Results of `fab_events`: {:?}", result);
 
 }
 
